e19ec47250b69554bc930e965218390495d10b8f
sonwh123 <80819521+sonwh123@users.noreply.github.com>
2023-07-04 21:10:27 +0900

    practical problem 2 finish

--- Greedy/practical_problem_2.py
+++ Greedy/practical_problem_2.py
@@ -1,17 +1,9 @@
 N, M = map(int, input().split())
-
+'''
 card = []
 for i in range(N):  #배열 입력
   card.append([])
   card[i] = list(map(int, input().split()))
-  # card[i] = []
-  # temp = []
-  # temp = list(map(int, input().split()))
-  # for j in range(M):
-  #   card[i][j] = temp[j]
-
-# for i in card:
-#   print(i)
 
 card_ = []  #각 행의 최솟값 모음
 
@@ -21,3 +13,15 @@ for i in range(N):
 
 card_.sort()  #각 행의 최솟값 정렬
 print(card_[-1])
+'''
+
+#min() 함수 사용
+result = 0
+for i in range(N):
+  card = list(map(int, input().split()))
+  card_min = min(card)
+  result = max(result, card_min)
+
+print(result)
+
+#이중 for문 사용(생략)
